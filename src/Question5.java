c32a1c72dff33ad4a8cdef1d7abe447512e225dc
Touyer Lee <touyer.lees@gmail.com>
2024-11-14 15:01:34 -0600

    Question 5

---
+++ src/Question5.java
@@ -0,0 +1,13 @@
+public class Question5 {
+    public static void main(String[] args) {
+//    	Write a program that declares two double variables, assigns a number to each, and divides the larger by the smaller number.
+//      Assign the result to a variable. Print out the result. Now, cast the result to an integer.
+//      Print out the result again.
+        double doubleFirst = 5.9;
+        double doubleSecond = 20.5;
+        int myInt = (int) doubleSecond;
+        double result = doubleSecond / doubleFirst;
+        System.out.println(result);
+
+    }
+}
