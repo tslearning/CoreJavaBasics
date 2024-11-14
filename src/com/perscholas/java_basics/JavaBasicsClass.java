047444083906b7862da896cd0e9ff06c09aacc84
Touyer Lee <touyer.lees@gmail.com>
2024-11-14 14:34:47 -0600

    Initial commit

---
+++ src/com/perscholas/java_basics/JavaBasicsClass.java
@@ -0,0 +1,60 @@
+package com.perscholas.java_basics;
+
+import java.sql.SQLOutput;
+
+public class JavaBasicsClass {
+    public static void main(String[] args) {
+//        int firstNum = 5;
+//        int secondNum = 4;
+//        int sum = firstNum + secondNum;
+//        System.out.println(sum);
+
+/////////add double////////////////////////////////
+//        double firstDecimal = 2.5;
+//        double secondDecimal = 5.9;
+//        double sumDecimal = firstDecimal + secondDecimal;
+//        System.out.println(sumDecimal);
+
+//////////add int and double///////////////////////
+//        int firstNumb = 7;
+//        double secondNumb = 7.7;
+//        double sum = firstNumb + secondNumb;
+//        System.out.println(sum);
+
+
+/////////must assign decimal value as a double/////////
+//        int firstNumber = 5;
+//        double secondNumber = 25.5;
+//        double result = secondNumber / firstNumber;
+//        System.out.println(result);
+
+
+/////Question 5
+//        double doubleFirst = 5.9;
+//        double doubleSecond = 20.5;
+//        int myInt = (int) doubleSecond;
+//        double result = doubleSecond / doubleFirst;
+//        System.out.println(result);
+
+
+///////Question 6
+//        int x = 5;
+//        int y =  (int) 6;
+//        double myDouble = (double) y;
+//        int q = y / x;
+//        System.out.println(q);
+
+/////////Question 7
+//        final int lastNum = 8;
+//        int result = lastNum * 8;
+//        System.out.println(result);
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/com/perscholas/java_basics/Question1.java
@@ -0,0 +1,13 @@
+package com.perscholas.java_basics;
+
+public class Question1 {
+    public static void main(String[] args) {
+    //Write a program that declares two integer variables, assigns an integer to each, and adds them together.
+    // Assign the sum to a variable. Print out the result.
+        int firstNum = 5;
+        int secondNum = 4;
+        int sum = firstNum + secondNum;
+        System.out.println(sum);
+    }
+
+}
